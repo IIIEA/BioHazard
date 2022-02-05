392c1e0b6812078ea0a715b968c32177abea089f
IIIEA <egor.solo2000@bk.ru>
2022-02-05 15:57:25 +0300

    Add plugin, setup main meu scene

---
+++ BioHazard/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using IJunior.TypedScenes;
+using System;
+
+public class SceneLoader : MonoBehaviour
+{
+    [Serializable]
+    public enum Scenes
+    {
+        MainMenu,
+    }
+
+    private Scenes _scene;
+
+    public void LoadScene()
+    {
+        switch (_scene)
+        {
+            case Scenes.MainMenu:
+                MainMenu.Load();
+                break;
+            default:
+                break;
+        }
+    }
+}
